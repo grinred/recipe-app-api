ad7d41120f5dc9f9f8de07a5b548247bd79e9476
Sergey Grinchuk <grinred69@gmail.com>
2024-05-14 17:59:20 +0300

    added db

--- app/app/calc.py
+++ app/app/calc.py
@@ -7,3 +7,7 @@ Calculator functions
 def add(x, y):
     # Add x and y and return result
     return x + y
+
+
+def subtract(x, y):
+    return y - x
--- app/app/tests.py
+++ app/app/tests.py
@@ -3,6 +3,11 @@ from app import calc
 
 
 class CalcTest(SimpleTestCase):
+
     def test_add_numbers(self):
         res = calc.add(5, 6)
         self.assertEquals(res, 11)
+
+    def test_subtract_numbers(self):
+        res = calc.subtract(10, 15)
+        self.assertEquals(res, 5)
