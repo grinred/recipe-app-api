8f203e9ca52cee9eba28d196c58fefb3c24cf4fb
Sergey Grinchuk <grinred69@gmail.com>
2024-05-21 20:09:27 +0300

    fixed errors

--- app/app/tests.py
+++ app/app/tests.py
@@ -6,8 +6,8 @@ class CalcTest(SimpleTestCase):
 
     def test_add_numbers(self):
         res = calc.add(5, 6)
-        self.assertEquals(res, 11)
+        self.assertEqual(res, 11)
 
     def test_subtract_numbers(self):
         res = calc.subtract(10, 15)
-        self.assertEquals(res, 5)
+        self.assertEqual(res, 5)
